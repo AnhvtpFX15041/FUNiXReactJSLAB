82d61684facde80827ec3ef75d273e057256dbae
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-02-02 23:32:39 +0700

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null
     };
   }
   onDishSelect(dishId) {
@@ -18,13 +19,19 @@ class Main extends Component {
     }
 
   render(){
+    const HomePage = () => {
+        return(
+            <Home />
+        );
+    }
     return (
       <div>
             <Header/>
-            <Menu dishes={this.state.dishes}
-                onClick={(dishId) => this.onDishSelect(dishId)} />
-            <DishDetail 
-                dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+            <Switch>
+                <Route path="/home" component={HomePage}/>
+                <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+                <Redirect to="/home" />
+            </Switch>
             <Footer/>
       </div>
     );
