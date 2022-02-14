383487cbc20bfe039706826d0dbfffa5c1ee256b
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-02-14 22:40:58 +0700

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -20,7 +20,7 @@ import { baseUrl } from '../shared/baseUrl';
             </div>
         )
     }
-    function RenderComments({comments, addComment, dishId}){
+    function RenderComments({comments, postComment, dishId}){
         if (comments!=null){
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -35,7 +35,7 @@ import { baseUrl } from '../shared/baseUrl';
                             );
                         })}
                     </div>
-                    <CommentForm dishId={dishId} addComment={addComment} />
+                    <CommentForm dishId={dishId} postComment={postComment} />
                 </div>     
             )
         } else {
@@ -67,7 +67,7 @@ import { baseUrl } from '../shared/baseUrl';
         }
         handleSubmit(values) {
             this.toggleModal();
-            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
         }
         render() {
             return(
@@ -162,7 +162,7 @@ import { baseUrl } from '../shared/baseUrl';
                     <div className="row">
                         <RenderDish dish = {props.dish}/>
                         <RenderComments comments={props.comments} 
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id}/>
                     </div>
                 </div>
