9882f11302d8d29ba8ab3a7e9c32e9d3596840cb
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-02-14 19:01:03 +0700

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,12 +5,13 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
     function RenderDish({dish}) {
         return(
             <div  className="col-12 col-md-5 m-1">
                 <Card>
-                    <CardImg width="100%" top src={dish.image} alt={dish.name} />
+                    <CardImg width="100%" top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
